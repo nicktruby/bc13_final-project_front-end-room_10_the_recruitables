f8c4e59884b8d6d1638a7a863a5cf5d8c61fc20e
Seb French <Seb@Sebs-MacBook-Pro.local>
2023-01-05 13:39:33 +0000

    new repo

--- cosmic-calc/src/App.js
+++ cosmic-calc/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar>
-        <Route exact path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/progress" element={<Progress />} />
         <Route path="/game" element={<Game />} />
