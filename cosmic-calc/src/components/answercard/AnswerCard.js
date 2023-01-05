cee3d7afa1aefc05534eb9eebb18b35d0429e173
jason_riley <jason.rileycode@hotmail.com>
2023-01-05 15:25:58 +0000

    Game page merged to main

---
+++ cosmic-calc/src/components/answercard/AnswerCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import NextQButton from "../buttons/NextQButton";
+
+export default function AnswerCard(props) {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      {/* <NextQButton onClick={props.handleClick} h1="Next Question!" /> */}
+    </div>
+  );
+}
--- cosmic-calc/src/components/functions/Functions.js
+++ cosmic-calc/src/components/functions/Functions.js
@@ -1,1 +1,18 @@
-// Tt
+function RandNumGen(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+export function timesTableCalculator(max) {
+    const randNum1 = RandNumGen(max);
+    const randNum2 = RandNumGen(max);
+    const randNumArr = [randNum1, randNum2];
+    return randNumArr;
+}
+
+export function multiply(arr) {
+    return arr[0] * arr[1];
+}
+
+export function checkAnswer(values, answer) {
+    return multiply(values) === answer;
+}
