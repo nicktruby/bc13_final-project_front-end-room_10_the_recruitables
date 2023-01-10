c4f917e70d2510c74bf67b7616914f33111d16c0
LouisWright <louis--wright@hotmail.co.uk>
2023-01-10 16:40:04 +0000

    added a switch statement for calculating the amount of levels that should be unlocked for a users score

--- cosmic-calc/src/pages/Progress.js
+++ cosmic-calc/src/pages/Progress.js
@@ -13,13 +13,14 @@ export const Progress = () => {
   const [score, setScore] = useState(0);
   // count for array of levels. New level pushed into array ever X amount of points. Then mapped below to return a new button each time score level reached.
   const [levels, setLevels] = useState([1]);
-  const [lockLevels, setLockLevels] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
-  const [payload, setPayload] = useState([])
-  const [totalScore, setTotalScore] = useState()
+  const [lockLevels, setLockLevels] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [payload, setPayload] = useState([]);
+  const [totalScore, setTotalScore] = useState();
+  let planets = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   // handles bringing in new buttons when the score increases. Set to new button every 5 points. Added button to manually increase score in the mean time - will remove later.
-  function handleScoreIncrease() {
 
+  function handleScoreIncrease() {
     // get request to get total_score
     const getScore = async (id) => {
       const response = await fetch(`http://localhost:4000/api/users/${id}`);
@@ -30,20 +31,45 @@ export const Progress = () => {
       return data.payload.total_score;
     };
 
-    getScore(5)
+    getScore(6);
 
-    // let newScore = score + 1;
-    // setScore(newScore);
-    // console.log(score);
-    if (totalScore === 0) {
-      return;
-    } else if (totalScore % 5 === 0) {
-      let newLevel = score + 1;
-      setLevels((addNewLevel) => [...addNewLevel, newLevel]);
-      console.log("this is levels array", levels);
+    switch (Math.round(totalScore / 100) * 100) {
+      case 100:
+        setLevels([1, 2]);
+        break;
+      case 200:
+        setLevels([1, 2, 3]);
+        break;
+      case 300:
+        setLevels([1, 2, 3, 4]);
+        break;
+      case 400:
+        setLevels([1, 2, 3, 4, 5]);
+        break;
+      case 500:
+        setLevels([1, 2, 3, 4, 5, 6]);
+        break;
+      case 600:
+        setLevels([1, 2, 3, 4, 5, 6, 7]);
+        break;
+      case 700:
+        setLevels([1, 2, 3, 4, 5, 6, 7, 8]);
+        break;
+      case 800:
+        setLevels([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        break;
+      case 900:
+        setLevels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        break;
+      case 1000:
+        setLevels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+        break;
+      default:
+        // Handle other cases here or do nothing
+        break;
     }
   }
-
+  // every 100 points or more new planet
   // set up the navigation variables and function
   const navigate = useNavigate();
   const navigateToGame = () => {
@@ -63,25 +89,15 @@ export const Progress = () => {
       <button onClick={handleScoreIncrease}>MANUAL SCORE INCREASE</button>
       <div className="grid-container">
         {levels.map((levels, index) => (
-
           <LevelButtons
             clickToGame={handleGotoLevel}
             key={levels.level}
-            ButtonNumber={index + 1 + '-unlock'}
-            
+            ButtonNumber={index + 1 + "-unlock"}
           />
         ))}
         {lockLevels.map((level, index) => (
-        <LevelButtons
-          
-          key={levels.level}
-          ButtonNumber={index + 1 + '-lock'}
-          
-          
-        />
-      ))}
-
-        
+          <LevelButtons key={levels.level} ButtonNumber={index + 1 + "-lock"} />
+        ))}
       </div>
     </div>
   );
