81a897d2e2558c684680907683c1d95b6d935817
jason_riley <jason.rileycode@hotmail.com>
2023-01-10 12:29:29 +0000

    Adaptive difficulty in progress

--- cosmic-calc/src/components/functions/Functions.js
+++ cosmic-calc/src/components/functions/Functions.js
@@ -14,3 +14,36 @@ export function multiply(arr) {
 export function checkAnswer(values, answer) {
   return multiply(values) === answer;
 }
+
+function valueChooser(chances) {
+  const valuesEasy = [0, 1, 2, 3, 5, 10];
+  const valuesMedium = [4, 6, 9, 11];
+  const valuesHard = [7, 8, 12];
+  const randomDifficulty = Math.random();
+  if (randomDifficulty < chances[0]) {
+    const randomIndex = Math.floor(Math.random() * valuesEasy.length);
+    return valuesEasy[randomIndex];
+  } else if (randomDifficulty < chances[1]) {
+    const randomIndex = Math.floor(Math.random() * valuesMedium.length);
+    return valuesMedium[randomIndex];
+  } else {
+    const randomIndex = Math.floor(Math.random() * valuesHard.length);
+    return valuesHard[randomIndex];
+  }
+}
+
+export function timesTableAdaptive(rank) {
+  let chances;
+  if (rank <= 1.75) {
+    chances = [0.6, 0.9]; // The first value is the chance of receiving easy values, the second is the chance of receiving easy or medium values
+  } else if (rank < 3.25) {
+    chances = [0.3, 0.9];
+  } else {
+    chances = [0.1, 0.6];
+  }
+  let chosenValues = [];
+  for (let i = 0; i < 1; i++) {
+    chosenValues.push(valueChooser(chances));
+  }
+  return chosenValues;
+}
